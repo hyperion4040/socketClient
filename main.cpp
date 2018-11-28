f2dd4517f931049848053bd94ac330a8f13b4422
hyperion4040 <hyperion1987@wp.pl>
2018-11-28 19:29:25 +0100

    Added new method

--- main.cpp
+++ main.cpp
@@ -7,6 +7,42 @@
 #include<arpa/inet.h> //inet_addr
 #include <zconf.h>
 
+
+int wyslijPlik(){
+    int sfd =0, n=0, b;
+    char rbuff[1024];
+    char sendbuffer[100];
+
+    struct sockaddr_in serv_addr;
+
+    memset(rbuff, '0', sizeof(rbuff));
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    b=connect(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (b==-1) {
+        perror("Connect");
+        return 1;
+    }
+
+    FILE *fp = fopen("prova.jpg", "rb");
+    if(fp == NULL){
+        perror("File");
+        return 2;
+    }
+
+    while( (b = fread(sendbuffer, 1, sizeof(sendbuffer), fp))>0 ){
+        send(sfd, sendbuffer, b, 0);
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+
 int main(int argc , char *argv[])
 {
     int sock;
@@ -37,8 +73,15 @@ int main(int argc , char *argv[])
     //keep communicating with server
     while(1)
     {
-        printf("Enter message : ");
+        printf("Wybierz 1 by wysłać tekst, wybierz 2 by wysłać plik : ");
         scanf("%s" , message);
+        if (message == "1"){
+            printf("Enter message");
+            scanf("%s",message);
+        } else{
+            wyslijPlik();
+            break;
+        }
 
         //Send some data
         if( send(sock , message , strlen(message) , 0) < 0)
