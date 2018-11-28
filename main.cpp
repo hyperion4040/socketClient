3cdef669a7552a822c96af824b788cd7f7d201b3
hyperion4040 <hyperion1987@wp.pl>
2018-11-28 20:45:46 +0100

    Added brute force version

--- main.cpp
+++ main.cpp
@@ -6,49 +6,17 @@
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
 #include <zconf.h>
-#include <fstream>
 
-
-int wyslijPlik(){
-    int sfd =0, n=0, b;
-    char rbuff[1024];
-    char sendbuffer[100];
-
-    struct sockaddr_in serv_addr;
-
-    memset(rbuff, '0', sizeof(rbuff));
-    sfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    b=connect(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    if (b==-1) {
-        perror("Connect");
-        return 1;
-    }
-    std::ofstream outfile("prova.jpg");
-
-
-
-    FILE *fp = fopen("prova.jpg", "rb");
-    if(fp == NULL){
-        perror("File");
-        return 2;
-    }
-
-    while( (b = fread(sendbuffer, 1, sizeof(sendbuffer), fp))>0 ){
-        send(sfd, sendbuffer, b, 0);
-    }
-
-    fclose(fp);
-    return 0;
-}
-
+int sendText();
+int sendFile();
 
 int main(int argc , char *argv[])
 {
+//    return sendText();
+    return sendFile();
+}
+
+int sendText() {
     int sock;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
@@ -77,15 +45,8 @@ int main(int argc , char *argv[])
     //keep communicating with server
     while(1)
     {
-        printf("Wybierz 1 by wysłać tekst, wybierz 2 by wysłać plik : ");
+        printf("Enter message : ");
         scanf("%s" , message);
-        if (message == "1"){
-            printf("Enter message");
-            scanf("%s",message);
-        } else{
-            wyslijPlik();
-            break;
-        }
 
         //Send some data
         if( send(sock , message , strlen(message) , 0) < 0)
@@ -106,5 +67,39 @@ int main(int argc , char *argv[])
     }
 
     close(sock);
+    return 0;
+}
+
+int sendFile(){
+    int sfd =0, n=0, b;
+    char rbuff[1024];
+    char sendbuffer[100];
+
+    struct sockaddr_in serv_addr;
+
+    memset(rbuff, '0', sizeof(rbuff));
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    b=connect(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (b==-1) {
+        perror("Connect");
+        return 1;
+    }
+
+    FILE *fp = fopen("prova.txt", "rb");
+    if(fp == NULL){
+        perror("File");
+        return 2;
+    }
+
+    while( (b = fread(sendbuffer, 1, sizeof(sendbuffer), fp))>0 ){
+        send(sfd, sendbuffer, b, 0);
+    }
+
+    fclose(fp);
     return 0;
 }
